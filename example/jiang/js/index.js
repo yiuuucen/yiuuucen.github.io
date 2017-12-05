500ffbff368d53663bc46ec059941c9342b9a263
yiuuucen <1784373887@qq.com>
2017-12-05 11:58:23 +0800

    work

--- example/jiang/js/index.js
+++ example/jiang/js/index.js
@@ -1,8 +1,10 @@
 //banner图效果
-function change_banner(){
+function change_banner(num){
     //获取当前显示图片的索引；
     //让下一张图片显示
-    var li_index=$(".tup li:visible").index();
+
+    var li_index = $(".tup li:visible").index();
+    console.log(li_index);
     var next_li_index=li_index+1;
     if(next_li_index==4){
         next_li_index=0
@@ -10,33 +12,53 @@ function change_banner(){
     $('.ann li').removeClass('dActive');
     $('.ann li').eq(next_li_index).addClass('dActive');
     $(".tup li").hide();
-    $(".tup li").eq(next_li_index).fadeIn();
+    $(".tup li").eq(next_li_index).fadeIn(0);
 
 };
 $(function(){
     //轮播初始化图片
     $('.tup li').hide();
-    $('.tup li').eq(0).fadeIn();
+    $('.tup li').eq(0).fadeIn(0);
     $('.ann li').removeClass('dActive');
     $('.ann li').eq(0).addClass('dActive');
 
     //悬浮事件
-    var an_interval=setTimeout(function(){
+    var ann_interval=null;
+
         $('.ann li').mouseover(function(){
             clearInterval(ban_interval);
-            clearInterval(an_interval);
+            clearInterval(ann_interval);
             var an_index=$(this).index();
             $('.ann li').removeClass('dActive');
             $('.ann li').eq(an_index).addClass('dActive');
             $('.tup li').hide();
-            $('.tup li').eq(an_index).fadeIn();
-        });
-        $('.ann li').mouseout(function(){
-            clearInterval(an_interval);
+            $('.tup li').eq(an_index).fadeIn(0);
+        })
+        $('.ann li').mouseleave(function(){
             clearInterval(ban_interval);
-            setInterval('change_banner()',3000);
+            clearInterval(ann_interval);
+            //这个定时器一直忘了清除，导致浪费3个小时！！！
+            ann_interval=setInterval('change_banner()',3000);
         })
         var ban_interval=setInterval('change_banner()',3000);
-    },1000)
 
+
+})
+//取得随机颜色
+var getRandomColor = function(){
+    return  '#' +
+        (function(color){
+            return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])
+            && (color.length == 6) ?  color : arguments.callee(color);
+        })('');
+}
+//随机颜色作用的nav的hover上
+$('.nav ul li a').mouseover(function(){
+    $('.nav ul li a:hover').css('color',getRandomColor)
+});
+$('.nav ul li a').mouseout(function(){
+    var This=this;
+    setTimeout(function(){
+        $(This).css('color','#fff');
+    },300)
 })
