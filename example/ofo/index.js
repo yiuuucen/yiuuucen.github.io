446db99dbfdafdd47ff36ae71308cb6f010a8fa4
yiuuucen <1784373887@qq.com>
2018-04-10 18:37:55 +0800

    work

---
+++ example/ofo/index.js
@@ -0,0 +1,50 @@
+setSize();
+function setSize(){
+    var desW =750;
+    var winW = document.documentElement.clientWidth;
+    if(winW>=750){
+        winW = 750;
+    }
+    document.documentElement.style.fontSize = (winW/desW * 100) + "px";
+}
+window.onresize = setSize;
+
+// 
+$(function(){
+	$(".up").click(function(){
+		$(".up").hide();
+		$(".msg").hide();
+		$(".upup").show();
+		$(".logo").css("height","0.8rem");
+	})
+	$(".upup").click(function(){
+		$(".up").show();
+		$(".msg").show();
+		$(".upup").hide();
+		$(".logo").css("height","0.7rem");
+	})
+	var flag=true;
+	$("#down").click(function(){
+		if(flag){
+			$("#user").slideUp();
+			$("#down").attr("src","foot-up.png");
+			flag=false;
+		}else{
+			$("#user").slideDown();
+			$("#down").attr("src","foot-down.png");
+			flag=true;
+		}
+		
+	})
+
+	$("#del").click(function(){
+		$(".shang").animate({top:'-25%'});
+		$(".xia").animate({top:'100%'});
+		
+	})
+	$("#geren").click(function(){
+		
+		$(".shang").animate({top:'0'});
+		$(".xia").animate({top:'25%'});
+	})
+})
